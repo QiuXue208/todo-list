1cfe7ce3b9d0ce0eebf94423b996d5462bc59a83
Charles <1309571292@qq.com>
2020-03-31 15:20:21 +0800

    代码优化: 使面向接口式

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const homedir = require("os").homedir();
-// 获取home变量, 如果读不到就取home目录
-const home = process.env.HOME || homedir;
-const path = require("path");
-const fs = require("fs");
-const dbPath = path.join(home, "todos");
+const db = require("./db.js");
 
-module.exports.add = tasks => {
+module.exports.add = async tasks => {
   // 读取之前的任务
-  fs.readFile(dbPath, { flag: "a+" }, (err, data) => {
-    let list;
-    if (err) {
-      // console.log(err);
-    }
-    try {
-      list = JSON.parse(data.toString());
-    } catch (err1) {
-      list = [];
-    }
-    // 将此次要添加的任务添加到读取的任务里
-    tasks.forEach(item => {
-      list.push({
-        title: item,
-        done: false
-      });
+  const list = await db.read();
+  // 将此次要添加的任务添加到读取的任务里
+  tasks.forEach(item => {
+    list.push({
+      title: item,
+      done: false
     });
-    // 将所有任务写进文件
-    fs.writeFile(dbPath, JSON.stringify(list), err2 => {});
   });
+  // 将所有任务写进文件
+  await db.write(list);
 };
