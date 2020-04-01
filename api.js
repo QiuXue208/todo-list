f71795db8202e77109fe96be63f7fe2181207fe4
Charles <1309571292@qq.com>
2020-04-01 15:15:05 +0800

    feat: 引入inquirer, 用交互式命令的方式完成一些操作

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const db = require("./db.js");
+const inquirer = require("inquirer");
 
 module.exports.add = async tasks => {
   // 读取之前的任务
@@ -20,9 +21,80 @@ module.exports.clear = async () => {
 
 module.exports.showAll = async () => {
   const list = await db.read();
-  if (list.length) {
-    list.map(item => {
-      console.log(`${item.done ? "[√]" : "[x]"} - ${item.title}`);
+
+  inquirer
+    .prompt({
+      type: "list",
+      name: "index",
+      message: "请选择你要进行的操作",
+      choices: [
+        { name: "退出", value: -1 },
+        { name: "创建任务", value: -2 },
+        ...list.map((item, index) => ({
+          name: `${item.done ? "[√]" : "[x]"} ${index + 1}. ${item.title}`,
+          value: index
+        }))
+      ]
+    })
+    .then(answers => {
+      const { index } = answers;
+      if (index === -2) {
+        // 创建任务
+        inquirer
+          .prompt({
+            type: "input",
+            name: "title",
+            message: "请输入任务名"
+          })
+          .then(answers => {
+            list.push({ title: answers.title, done: false });
+            db.write(list);
+          });
+      } else if (index >= 0) {
+        // 选中任务
+        inquirer
+          .prompt({
+            type: "list",
+            name: "action",
+            message: "请选择你要进行的操作",
+            choices: [
+              { name: "退出", value: "quit" },
+              { name: "标记为完成", value: "markAsDone" },
+              { name: "标记为未完成", value: "markAsUndone" },
+              { name: "删除", value: "delete" },
+              { name: "编辑", value: "edit" }
+            ]
+          })
+          .then(answers => {
+            const { action } = answers;
+            switch (action) {
+              case "markAsDone":
+                list[index].done = true;
+                db.write(list);
+                break;
+              case "markAsUndone":
+                list[index].done = false;
+                db.write(list);
+                break;
+              case "delete":
+                list.splice(index, 1);
+                db.write(list);
+                break;
+              case "edit":
+                inquirer
+                  .prompt({
+                    type: "input",
+                    name: "title",
+                    message: "请输入任务名",
+                    default: list[index].title
+                  })
+                  .then(answers => {
+                    list[index].title = answers.title;
+                    db.write(list);
+                  });
+                break;
+            }
+          });
+      }
     });
-  }
 };
